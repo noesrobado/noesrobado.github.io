b33d8d5431d0f5d1ca01802f3495514324bd9fcf
Germán Aliprandi <galiprandi@gmail.com>
2021-05-28 19:43:59 -0300

    fix: :bug: Firebase config

--- src/Services/firebase.ts
+++ src/Services/firebase.ts
@@ -6,18 +6,18 @@ import { getFirestore } from "firebase/firestore"
 dotenv.config()
 
 // Initialize Firebase
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
-};
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_DATABASE_URL,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+//   measurementId: process.env.REACT_APP_MEASUREMENT_ID
+// };
 
-var firebaseConfig2 = {
+var firebaseConfig = {
   apiKey: "AIzaSyCaBB95IOyfGTCuRnLFrtg3KQiL4myiGA4",
   authDomain: "noesrobado-app.firebaseapp.com",
   databaseURL: "https://noesrobado-app-default-rtdb.firebaseio.com",
@@ -27,7 +27,7 @@ var firebaseConfig2 = {
   appId: "1:96848561278:web:1173f14988816e4573442f",
   measurementId: "G-Y31PM071CX"
 };
-export const firebaseApp = initializeApp(firebaseConfig2);
+export const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore();
 
 
