0ac5fb15342da16e4007e727ce7c1970398c679c
German Aliprandi <galiprandi@gmail.com>
2021-06-14 11:56:44 -0300

    feat: :sparkles: Change route to private

--- src/index.tsx
+++ src/index.tsx
@@ -15,11 +15,13 @@ import { ProductsProvider } from './Hooks/useProducts'
 // Components
 import { Home } from './Pages/Home/Home'
 import { Productos } from './Pages/Productos/Productos'
+import { Consultas } from './Pages/Consultas/Consultas'
 import { Nuevo } from './Pages/Productos/Nuevo/Nuevo'
 import { Detalles } from './Pages/Productos/Detalles/Detalles'
 import { Transferir } from './Pages/Productos/Transferir/Transferir'
 import { ComoFunciona } from './Pages/ComoFunciona/ComoFunciona'
 import { Login } from './Pages/Login/Login'
+import { PrivateRoute } from './Components/PrivateRoute'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -28,14 +30,15 @@ ReactDOM.render(
         <Router>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/productos/nuevo" component={Nuevo} />
-            <Route
+            <PrivateRoute exact path="/consultas/:key" component={Consultas} />
+            <PrivateRoute exact path="/productos/nuevo" component={Nuevo} />
+            <PrivateRoute
               exact
               path="/productos/transferir/:id"
               component={Transferir}
             />
-            <Route exact path="/productos/" component={Productos} />
-            <Route exact path="/producto/:id" component={Detalles} />
+            <PrivateRoute exact path="/productos/" component={Productos} />
+            <PrivateRoute exact path="/producto/:id" component={Detalles} />
             <Route exact path="/como-funciona/" component={ComoFunciona} />
             <Route exact path="/login" component={Login} />
           </Switch>
